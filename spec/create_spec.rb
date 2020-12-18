daf27d4d73be97ba5ac8d3d54afe03c1549e629f
Your Username <you@example.com>
2020-12-18 17:37:37 -0500

    commit tree 12433731daa5529f151e77cddd1cb44fdfd5ac07

--- spec/create_spec.rb
+++ spec/create_spec.rb
@@ -7,7 +7,7 @@ describe 'creating bears table' do
 
   it "creates a bears table with a name field" do
     expect{@db.execute("SELECT name FROM bears;")}.to_not raise_exception
-  end
+  end 
 
   it "creates a bears table with a age field" do
     expect{@db.execute("SELECT age FROM bears;")}.to_not raise_exception
